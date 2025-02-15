25300cafb65dcc1a61c5d62307c34c48ca6ff368
Rolazar <rolazarcoding@gmail.com>
2025-02-15 15:38:30 -0800

    testing pushState

--- redirect.js
+++ redirect.js
@@ -1,4 +1,5 @@
 const home = "/react-navigation";
 const path = window.location.pathname;
-setTimeout(() => window.history.pushState("", "", path), 3000);
-window.location.replace(home);
+setTimeout(() => window.history.pushState("", "", home), 3000);
+// setTimeout(() => window.history.pushState("", "", path), 3000);
+// window.location.replace(home);
